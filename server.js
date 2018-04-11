6d2f5816c023a9ec9c83662e9c2c198378e1d51a
jinx73 <tayjxn@gmail.com>
2018-04-11 11:14:08 +0800

    added server side authentication for submitting files

--- server.js
+++ server.js
@@ -33,16 +33,16 @@ var registerPassport = function (client, params) {
 
 	app.get('/login', passport.authenticate('oidc',
 	  {
-		successRedirect: '/home',
-		failureRedirect: '/',
+		successRedirect: '/',
+		failureRedirect: '/login',
 		failureFlash: true,
 	  }
 	));
 
 	app.get('/callback', passport.authenticate('oidc',
 	  {
-		successRedirect: '/home',
-		failureRedirect: '/',
+		successRedirect: '/',
+		failureRedirect: '/login',
 		failureFlash: true,
 	  }
 	));
@@ -117,19 +117,33 @@ con.connect(function(err){
 
 //routes=======================================================================
 
+app.get('/', ensureAuthenticated, function(req, res){
+	res.render("index.ejs");
+})
+
 app.get('/home', ensureAuthenticated, function(req, res){
-	var sql = "SELECT p_title, p_code, p_status, p_org, S1_Name, S2_Name, S3_Name, S4_Name FROM projects WHERE S1_NAME = '" + req.user.name +"' ";
-	sql += "OR S2_NAME = '" + req.user.name +"'";
-	sql += "OR S3_NAME = '" + req.user.name +"'";
-	sql += "OR S4_NAME = '" + req.user.name +"'";
+	var sql = "SELECT p_title, p_code, p_status, p_org, S1_Name, S2_Name, S3_Name, S4_Name FROM projects WHERE S1_Name = '" + req.user.Name +"' ";
+	sql += "OR S2_Name = '" + req.user.name +"'";
+	sql += "OR S3_Name = '" + req.user.name +"'";
+	sql += "OR S4_Name = '" + req.user.name +"'";
 	con.query(sql, function(err, results, fields){
 		if(err) throw err;
-		//console.log(results);
-		res.render("index.ejs", {projects: results});
+		console.log(results);
+		res.render("home.ejs", {projects: results});
 	});
 })
 
-app.get('/project/:projectCode', ensureAuthenticated, function(req, res){/* WHERE p_code = '" + req.params.projectCode + "'*/
+app.get('/all', ensureAuthenticated, function(req, res){
+	var sql = "SELECT projects.*, grading.status, grading.report, grading.poster FROM projects INNER JOIN grading ON projects.p_code = grading.p_code";
+	console.log(sql);
+	con.query(sql, function(err, results){
+		if(err) throw err;
+		console.log(results[0]);
+		res.render("all.ejs", {projects: results});
+	});
+})
+
+app.get('/project/:projectCode', ensureAuthenticated, function(req, res){
 	var sql = "SELECT projects.*, grading.p_code, grading.status, grading.report, grading.poster FROM projects INNER JOIN grading ON projects.p_code=grading.p_code WHERE projects.p_code = '" + req.params.projectCode + "'";
 	console.log(sql);
 	con.query(sql, function(err, results){
@@ -138,6 +152,109 @@ app.get('/project/:projectCode', ensureAuthenticated, function(req, res){/* WHER
 		res.render('project.ejs', {project: results, user: req.user.name});
 	})
 });
+
+app.get('/stop', function(req, res){
+	res.render("funny.ejs");
+})
+
+app.post('/uploadDocs', ensureAuthenticated, function(req, res){
+	var form = new formidable.IncomingForm();
+	var c = "";
+	var fpath = "";
+	var sql1 = "", sql2 = "";
+	var validate = false;
+	var str = ['S1_Name', 'S2_Name', 'S3_Name', 'S4_Name'];
+	form.parse(req);
+	form.on('field', function(name, value){
+		if(name == 'code'){
+			c = value;
+			con.query("SELECT S1_Name, S2_Name, S3_Name, S4_Name FROM projects WHERE p_code = '" + c + "'", function(err, results){
+				for(var i = 0; i < 4; i++){
+					if(results[0][str[i]] == req.user.name){
+						validate = true;
+						break;
+					}
+				}
+				if(!validate){
+					res.redirect("/stop");
+				}
+			});
+		}
+		if(name == 'pStatus'){
+			if(value == 'Ready to Grade at RC'){
+				fpath = "/uploads/Report/" + c + "_report.pdf";
+				sql1 = "UPDATE grading SET status = '" + value + "', report = '" + c + "_report.pdf' WHERE p_code = '" + c + "'";
+			}else if(value == 'Extension Required'){
+				fpath = "/uploads/ExtForm/" + c + "_ext.pdf";
+				sql1 = "UPDATE grading SET status = '" + value + "', report = '" + c + "_ext.pdf' WHERE p_code = '" + c + "'";
+			}
+		}
+	});
+
+	form.on('fileBegin', function (name, file){
+			file.path = __dirname + fpath;
+	});
+
+	form.on('file', function (name, file){
+			console.log('Uploaded ' + file.name);
+	});
+
+	form.on('end', function(){
+		if(validate){
+			con.query(sql1, function(err, result){
+				if(err) throw err;
+				console.log(result);
+			})
+			res.redirect('/project/'+ c);
+		}
+	})
+
+
+
+});
+
+app.post('/uploadPoster', ensureAuthenticated, function(req, res){
+	var form = new formidable.IncomingForm();
+	form.parse(req);
+	var c = "";
+	var sql = "";
+	var validate = false;
+	var str = ['S1_Name', 'S2_Name', 'S3_Name', 'S4_Name'];
+	form.on('field', function(name, value){
+		if(name == 'code'){
+			c = value;
+			con.query("SELECT S1_Name, S2_Name, S3_Name, S4_Name FROM projects WHERE p_code = '" + c + "'", function(err, results){
+				for(var i = 0; i < 4; i++){
+					if(results[0][str[i]] == req.user.name){
+						validate = true;
+						break;
+					}
+				}
+				if(!validate){
+					res.redirect("/stop");
+				}
+			});
+		}
+		sql = "UPDATE grading SET poster = '" + value +"_poster.pdf' WHERE p_code = '" + value + "'";
+		console.log(sql);
+	});
+	form.on('fileBegin', function (name, file){
+    	file.path = __dirname + '/uploads/Poster/' + c + '_poster.pdf';
+  });
+	form.on('end', function(){
+		if(validate){
+			con.query(sql, function(err, res){
+				if(err) throw err;
+				console.log(res);
+			});
+			res.redirect('/project/' + c);
+		}
+	});
+})
+
+app.get('/download/:type/:projectCode', function(req, res){
+	res.download(__dirname + "/uploads/" + req.params.type + "/" + req.params.projectCode);
+})
 
 //=============================================================================
 
